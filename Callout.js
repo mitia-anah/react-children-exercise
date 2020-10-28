c2ec787dbb3f465f0b86d79c0e951937bd64f6f7
Rosny Ratsianompo <rosny.rat@onja.org>
2020-10-28 07:11:53 +0000

    added react-children

---
+++ Callout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Callout({ children }) {
+    return (
+        <div className="callout">
+            {children}
+        </div>
+    )
+}
+export default Callout
